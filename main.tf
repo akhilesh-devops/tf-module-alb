2199d2547c11be0b02f99903448b5149f262972e
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-18 23:58:30 +0530

    test

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "main" {
+  name               = "${var.env}-alb"
+  internal           = var.internal
+  load_balancer_type = var.lb_type
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.env}-alb-sg"
+  description = "${var.env}-alb-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = aws_vpc.main.cidr_block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "tags" {}
+variable "env" {}
+variable "internal" {}
+variable "lb_type" {}
+variable "vpc_id" {}
